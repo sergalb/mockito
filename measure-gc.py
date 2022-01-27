f91182bd7ca3a393f207866bfad1c61986a7f162
Sergey Balakhnin <Sergey.Balakhnin@jetbrains.com>
2022-01-27 19:32:07 +0300

    [qodana] QD-2058 add measure gc script

---
+++ measure-gc.py
@@ -0,0 +1,32 @@
+import os
+import time
+import statistics
+
+
+def measureDockerRun(imageNumber):
+    start = time.time()
+    os.system("docker run  --rm " +
+              "-v $(pwd):/data/project " +
+              "-v $(pwd)/out:/data/results " +
+              "-p 10001:10001 repo.labs.intellij.net/static-analyser/qodana:" + imageNumber
+              )
+    end = time.time()
+    return end - start
+
+
+def measureGc(gc):
+    times = []
+    for i in range(100):
+        times.append(measureDockerRun(gc, "yii2"))
+    return times
+
+
+parallelResults = measureGc("20868.2523")
+g1Results = measureGc("20868.2521")
+zResults = measureGc("20868.2526")
+results = [
+           ["G1", statistics.mean(g1Results), statistics.stdev(g1Results), g1Results],
+           ["Parallel ", statistics.mean(parallelResults), statistics.stdev(parallelResults), parallelResults],
+           # ["Z", statistics.mean(zResults), statistics.stdev(zResults)],
+           ]
+print(results)
